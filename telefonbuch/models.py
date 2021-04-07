842ab321fb3a6bd75695b86cce405616059361aa
Lauritz Engelmeier <lauritz.engelmeier@posteo.de>
2021-04-07 18:40:51 +0200

    whatever

---
+++ telefonbuch/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class TelephoneBookEntry(models.Model):
+
+    name = models.CharField(max_length=30)
+    number = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
---
+++ telefonbuch/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import TelephoneBookEntry
+# Create your views here.
+
+
+def hello(request):
+    return HttpResponse("Hello, World!")
+
+def entries(request):
+    entries = TelephoneBookEntry.objects.all()
+    return render(request, 'entries.html', context={'entries': entries})
